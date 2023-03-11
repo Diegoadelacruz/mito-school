5f76766753a9b36ca9a6400712277f6c971204d1
Diego de la Cruz <diego.dlacruz@gmail.com>
2023-03-11 09:16:22 -0600

    Base Project with Initial Java and Spring Structure

---
+++ src/main/java/com/mitocode/mitoschool/MitoSchoolApplication.java
@@ -0,0 +1,13 @@
+package com.mitocode.mitoschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MitoSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MitoSchoolApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mitocode/mitoschool/MitoSchoolApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mitocode.mitoschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MitoSchoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
