7282a54585e14433d4b9261735f4f3eeb9ea2fb6
Diego de la Cruz <diego.dlacruz@gmail.com>
2023-03-12 11:57:47 -0600

    Adding missing persistence to class models, project running and creating db tables succesfully

--- src/main/java/com/mitocode/mitoschool/model/Enrollment.java
+++ src/main/java/com/mitocode/mitoschool/model/Enrollment.java
@@ -1,14 +1,36 @@
 package com.mitocode.mitoschool.model;
 
+import jakarta.persistence.CascadeType;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ForeignKey;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToMany;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
 import java.time.LocalDateTime;
 import java.util.List;
 
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
 public class Enrollment {
 
+    @Id
+    @Column(nullable = false)
     private LocalDateTime enrollDate;
-
+    @ManyToOne
+    @JoinColumn(name = "id_student", nullable = false, foreignKey = @ForeignKey(name = "FK_Enrollment"))
     private Student student;
 
+    @OneToMany(mappedBy = "enrollment", cascade = CascadeType.ALL)
     private List<EnrollmentDetail> details;
 
     private boolean status;
--- src/main/java/com/mitocode/mitoschool/model/Student.java
+++ src/main/java/com/mitocode/mitoschool/model/Student.java
@@ -10,17 +10,22 @@ import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
-
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+@Entity
 public class Student {
-
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer idStudent;
-
+    @Column(length = 50, nullable = false)
     private String name;
-
+    @Column(length = 50, nullable = false)
     private String lastname;
-
+    @Column(length = 10, nullable = false)
     private String dni;
-
+    @Column(length = 3, nullable = false)
     private int age;
 
 }
